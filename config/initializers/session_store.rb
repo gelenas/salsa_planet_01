784aff84d673abb8df4b2f69473e53e109110b58
GS <gelenas@hotmail.com>
2013-12-02 22:28:51 -0800

    Bootstrap 3 added

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-SampleApp::Application.config.session_store :cookie_store, key: '_sample_app_session'
+SalsaPlanet01::Application.config.session_store :cookie_store, key: '_salsa_planet_01_session'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-SampleApp::Application.routes.draw do
+SalsaPlanet01::Application.routes.draw do
   
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
